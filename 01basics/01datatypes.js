c81bbdfe4c28158534f517c1f7d3182185dec973
hameer mohd khan <128599632+hameerkhokhar@users.noreply.github.com>
2024-03-15 06:52:20 +0000

    datatypes

---
+++ 01basics/01datatypes.js
@@ -0,0 +1,17 @@
+let name="Hameer"
+let age=22
+let isloggedin=false
+// Data dtyes
+
+// number 2 to power 53
+// bigint 
+// boolean flase/true
+//  string ""
+// null => stand alone value
+// undefined
+// symbol
+// object
+
+console.log(typeof "Hameer");
+console.log(typeof age);
+console.log(typeof null);
